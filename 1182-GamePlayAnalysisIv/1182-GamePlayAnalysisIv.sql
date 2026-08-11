941613afc9151ed2d8f6c1f45f46ff34269bb6f9
divyaa-Debug <divyadd090207@gmail.com>
2026-08-11 16:09:21 +0530

    Create 1182-GamePlayAnalysisIv

---
+++ 1182-GamePlayAnalysisIv/1182-GamePlayAnalysisIv.sql
@@ -0,0 +1,19 @@
+-- Last updated: 11/08/2026, 16:09:20
+# Write your MySQL query statement below
+SELECT 
+    ROUND(
+        COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 
+        2
+    ) AS fraction
+FROM 
+    Activity
+WHERE 
+    (player_id, DATE_SUB(event_date, INTERVAL 1 DAY)) IN (
+        SELECT 
+            player_id, 
+            MIN(event_date)
+        FROM 
+            Activity
+        GROUP BY 
+            player_id
+    );
