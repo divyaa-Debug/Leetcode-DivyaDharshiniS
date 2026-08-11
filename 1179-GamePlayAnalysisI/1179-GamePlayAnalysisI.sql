fc3e12bbb0ad81fc5714db72c36a21b4adc06ae3
divyaa-Debug <divyadd090207@gmail.com>
2026-08-11 16:09:24 +0530

    Create 1179-GamePlayAnalysisI

---
+++ 1179-GamePlayAnalysisI/1179-GamePlayAnalysisI.sql
@@ -0,0 +1,10 @@
+-- Last updated: 11/08/2026, 16:09:23
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT 
+    player_id, 
+    MIN(event_date) AS first_login
+FROM 
+    Activity
+GROUP BY 
+    player_id;
