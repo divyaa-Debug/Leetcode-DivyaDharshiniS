e7cbb03195dc81e7386a7334a19a3a30fe5b098f
divyaa-Debug <divyadd090207@gmail.com>
2026-08-11 16:12:59 +0530

    Create 206-ReverseLinkedList

---
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -0,0 +1,15 @@
+// Last updated: 11/08/2026, 16:12:57
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode node = null;
+
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = node;
+            node = head;
+            head = temp;
+        }
+
+        return node;        
+    }
+}
