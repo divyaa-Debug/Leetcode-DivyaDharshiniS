a4ffbc703aac665716016bdbf482e81d0c9c8978
divyaa-Debug <divyadd090207@gmail.com>
2026-08-31 09:49:40 +0530

    Create 191-Numberof1Bits

---
+++ 191-Numberof1Bits/version/java/191-Numberof1Bits_20260831_094940.java
@@ -0,0 +1,11 @@
+// Last updated: 31/08/2026, 09:49:40
+1public class Solution {
+2     public int hammingWeight(int n) {
+3        int count = 0;
+4        while (n != 0) {
+5            n &= (n - 1);  
+6            count++;
+7        }
+8        return count;
+9    }
+10}
